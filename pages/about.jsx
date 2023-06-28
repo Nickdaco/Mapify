0b955fcb1d87b3f7bacec100cf40ecfbe3b376cf
nickmamisashvili <mamisashvili.n@northeastern.edu>
2023-06-28 01:19:44 -0400

    about page wip

--- pages/about.jsx
+++ pages/about.jsx
@@ -25,7 +25,7 @@ export default function Home() {
       <Heading>FAQ</Heading>
       <Heading size='md'>Why cant I view artits with overlapping locations?</Heading>
       <Text>Right now thats a work in progress</Text>
-      <Heading size='md'>I've noticed some of my top artists are missing! Why is that?</Heading>
+      <Heading size='md'>Ive noticed some of my top artists are missing! Why is that?</Heading>
       <Text>Theres two reasons this could be happening, 1: The artist isnt on Wikipedia and/or cant be parsed on wikipedia. 2: The geocoding API im using cant find the location. 
         In both of these cases im working on a fix
       </Text>
